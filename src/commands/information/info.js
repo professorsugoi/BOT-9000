8f196782931819e5ba328af8eb3bb26041f750eb
professorsugoi <emosock@live.com>
2023-03-15 12:14:37 -0400

    !info, !info user working

--- src/commands/information/info.js
+++ src/commands/information/info.js
@@ -1,19 +1,77 @@
+const infoUser = require('./sub');
+
 /**
  * @type {import("@structures/Command")}
  */
 module.exports = {
 	name: 'info',
-	description: 'displays info commands',
+	description: 'show various information',
 	category: 'INFORMATION',
+	botPermissions: ['EmbedLinks'],
 	command: {
 		enabled: true,
+		subcommands: [
+			{
+				trigger: 'user',
+				description: 'get info for a user',
+			},
+			// {
+			// 	trigger: 'channel <channel>',
+			// 	description: 'test',
+			// },
+		],
 	},
 
 	async messageRun(message, args) {
-		await message.safeReply(`codehere`);
-	},
+		const sub = args[0].toLowerCase();
+		if (!sub) return message.safeReply('Use `!info [query]` to get information about a query.');
 
-	async interactionRun(interaction) {
-		await interaction.followUp(`codehere`);
+		let response;
+
+		// user
+		if (sub === 'user') {
+			const target = args[1] ? await message.guild.resolveMember(args[1]) : message.member;
+			if (!target) {
+				return message.safeReply('User not found. Please provide a valid user.');
+			}
+			response = infoUser(target);
+		} else {
+			return message.safeReply(`\`${args[0]}\` is not a valid subcommand.`);
+		}
+
+		// // channel
+		// else if (sub === 'channel') {
+		// 	const target = message.mentions.channels.first() || message.channel;
+		// 	response = await info_channel(target.channel);
+		// }
+
+		// // guild
+		// else if (sub === 'guild' || sub === 'server') {
+		// 	response = await info_guild(message.guild);
+		// }
+
+		// // bot
+		// else if (sub === 'bot') {
+		// 	response = info_bot(message.client);
+		// }
+
+		// // avatar
+		// else if (sub === 'avatar' || sub === 'pfp') {
+		// 	const target = message.mentions.members.first() || message.member;
+		// 	response = info_avatar(target);
+		// }
+
+		// // emoji
+		// else if (sub === 'emoji') {
+		// 	let emoji = message.options.getString('name');
+		// 	response = info_emoji(emoji);
+		// }
+
+		// return
+		// else {
+		// 	response = 'Incorrect subcommand';
+		// }
+
+		await message.safeReply(response);
 	},
 };
---
+++ src/commands/information/sub/infoUser.js
@@ -0,0 +1,81 @@
+const { EmbedBuilder } = require('discord.js');
+const { EMBED_COLORS } = require('@root/config');
+
+/**
+ * @type {import("@structures/Command")}
+ */
+module.exports.info = {
+	name: 'user',
+	description: 'shows information about a user',
+	usage: '[@member|id]',
+	aliases: ['uinfo', 'memberinfo'],
+};
+
+/**
+ * @param {import('discord.js').GuildMember} member
+ */
+module.exports = (member) => {
+	let color = member.displayHexColor;
+	if (color === '#000000') color = EMBED_COLORS.BOT_EMBED;
+
+	let filteredRoles = member.roles.cache
+		.filter((r) => r.name !== '@everyone');
+	let rolesString = member.roles.cache
+		.filter((r) => r.name !== '@everyone')
+		.map((r) => r.toString())
+		.join(' | ');
+	if (rolesString.length > 1024) rolesString = rolesString.substring(0, 1020) + '...';
+
+	const embed = new EmbedBuilder()
+		.setAuthor({
+			name: `USER INFO ☆`,
+			iconURL: member.user.displayAvatarURL(),
+		})
+		.setThumbnail(member.user.displayAvatarURL())
+		.setColor(color)
+		.addFields(
+			{
+				name: 'Nickname',
+				value: member.displayName,
+				inline: true,
+			},
+			{
+				name: 'Usertag',
+				value: member.user.tag,
+				inline: true,
+			},
+			{
+				name: '\u200B',
+				value: '\u200B',
+				inline: true,
+			},
+			{
+				name: 'Joined server',
+				value: member.joinedAt.toLocaleDateString(),
+				inline: true,
+			},
+			{
+				name: 'Account created',
+				value: member.user.createdAt.toLocaleDateString(),
+				inline: true,
+			},
+			{
+				name: '\u200B',
+				value: '\u200B',
+				inline: true,
+			},
+			{
+				name: `Roles [${filteredRoles.size}]`,
+				value: rolesString,
+				inline: true,
+			}
+			// {
+			// 	name: 'Avatar',
+			// 	value: member.user.displayAvatarURL({ extension: 'png' }),
+			// }
+		)
+		.setTimestamp(Date.now());
+
+	return { embeds: [embed] };
+};
+
